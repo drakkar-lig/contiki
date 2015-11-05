sics-iot/calipso-integrated

Random wait before re-transmitting RREQ and then RREP Improved condition for taking into account new OPT
revert add print route entry size

core/net/uip-ds6-route.c
@@ -30,7 +30,7 @@
  *
  */
  
-#if WITH_IPV6_LOADNG 
+#if WITH_IPV6_RRPL
 #include "net/rrpl/rrpl.h"
 #endif
 #include "net/uip-ds6.h"
@@ -343,6 +343,7 @@ uip_ds6_route_add(uip_ipaddr_t *ipaddr, uint8_t length,
   PRINT6ADDR(ipaddr);
   PRINTF(" via ");
   PRINT6ADDR(nexthop);
+  PRINTF(" prefix len:%u", r->length);
   PRINTF("\n");
   ANNOTATE("#L %u 1;blue\n", nexthop->u8[sizeof(uip_ipaddr_t) - 1]);
 

drakkar-lig/contiki

core/net/ipv6/uip-ds6-route.c
@@ -41,7 +41,7 @@
 #include "net/ipv6/uip-ds6.h"
 #include "net/ip/uip.h"
 
-#if WITH_IPV6_LOADNG
+#if WITH_IPV6_RRPL
 #include "net/rrpl/rrpl.h"
 #endif
 
@@ -384,6 +384,7 @@ uip_ds6_route_add(uip_ipaddr_t *ipaddr, uint8_t length,
   PRINT6ADDR(ipaddr);
   PRINTF(" via ");
   PRINT6ADDR(nexthop);
+  PRINTF(" prefi len:%u", r->length);
   PRINTF("\n");
   ANNOTATE("#L %u 1;blue\n", nexthop->u8[sizeof(uip_ipaddr_t) - 1]);
 