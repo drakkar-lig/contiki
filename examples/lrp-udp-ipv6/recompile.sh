58aeac888906a0b1514a28fe08d5bc5bc9948f63
Henry-Joseph Audéoud <henry-joseph.audeoud@imag.fr>
2016-02-02 15:14:58 +0100

    [FIX] LRP example recompilation script

--- examples/lrp-udp-ipv6/recompile.sh
+++ examples/lrp-udp-ipv6/recompile.sh
@@ -2,50 +2,34 @@
 #
 
 # Move to this example's directory
-OLD_WD="$(pwd)"
 cd "$(dirname "$0")"
 
-if test -f Makefile.target; then
-  EXTENSION="$(cat Makefile.target | cut -d' ' -f3)"
-else
-  EXTENSION=sky
+# Set TARGET variable correctly
+if test -z "$TARGET"; then
+  if test -f Makefile.target; then
+    TARGET="$(cat Makefile.target | cut -d' ' -f3)"
+  else
+    TARGET=sky
+  fi
 fi
-OBJECT_FILES=$(echo obj_${EXTENSION}/{tcpip,lrp*}.{d,o})
-EXECUTABLES=$(echo ./{relay,udp-client,udp-server}.${EXTENSION})
 
-if ! make -q udp-server; then
-  echo "Compiling udp-server"
+# Files we do not want to keep after compilation, because they contain
+# compiled code that depend on LRP_IS_SINK and LRP_IS_COORDINATOR
+# macros.
+LRP_OBJECT_FILES=$(echo obj_${TARGET}/lrp*.{d,o})
+
+# Compile code, but remove LRP object files just after
+if ! make -q udp-server >/dev/null; then
   make udp-server || exit 1
-  rm -f $OBJECT_FILES
+  rm -f $LRP_OBJECT_FILES
 fi
 
-if ! make -q udp-client; then
-  echo "Compiling udp-client"
+if ! make -q udp-client >/dev/null; then
   make udp-client || exit 1
-  rm -f $OBJECT_FILES
+  rm -f $LRP_OBJECT_FILES
 fi
 
-if ! make -q relay; then
-  echo "Compiling relay"
+if ! make -q relay >/dev/null; then
   make relay || exit 1
-  rm -f $OBJECT_FILES
-fi
-
-touch $EXECUTABLES
-
-# First argument (optionnal) describe the directory where we have to put
-# executables
-if [ "$1" != "" ]; then
-  # Destination directory is relative to OLD_WD
-  if test "${1:0:1}" = /; then
-    DIR="$1"
-  else
-    DIR="$OLD_WD/$1"
-  fi
-  if [ -d "$DIR" ]; then
-    echo Moving executables to "'$1'"
-    cp ${EXECUTABLES} "$DIR"/
-  else
-    echo >&2 "'$1' n'est pas un dossier…"
-  fi
+  rm -f $LRP_OBJECT_FILES
 fi
